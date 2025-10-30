f1ead6261fdeb4b451179f02b9d69e9310e51c29
KunalTanwar130 <kunaldropshipping13@gmail.com>
2025-10-30 13:33:37 +0530

    Create product.js

---
+++ models/product.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Product name is required"],
+  },
+  price: {
+    type: Number,
+    required: [true, "Price is required"],
+    min: [0, "Price cannot be negative"],
+  },
+  category: {
+    type: String,
+    required: [true, "Category is required"],
+  },
+});
+
+module.exports = mongoose.model("Product", productSchema);
